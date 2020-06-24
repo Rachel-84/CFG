dd742c7cbc247a615b02296f8c3117c7d526534b
Rachel-84 <64856101+Rachel-84@users.noreply.github.com>
2020-06-24 10:43:17 +0100

    CFG
    CFG

---
+++ javascript.js
@@ -0,0 +1,7 @@
+var blues = prompt("Do you have the holiday blues? Please answer 'yes' or 'no'");
+
+if(blues =='yes'){
+    alert("Warning! This page might not be for you...")
+} if(blues =='no'){
+    alert("I hope this page does not give you them...")
+} 
